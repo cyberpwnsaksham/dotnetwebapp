4633821c030d5af6a5d334b391a35a4f92689d4a
cyberpwnsaksham <144447583+cyberpwnsaksham@users.noreply.github.com>
2023-09-29 09:44:10 +0530

    Add files via upload

---
+++ dotnet.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+
+    environment {
+        // Define environment variables as needed
+        DOTNET_VERSION = '6.0' // Change to your desired .NET version
+        DOCKER_IMAGE_NAME = 'my-dotnet-app'
+        DOCKER_REGISTRY_URL = 'bsaksham/dotnetwebapp:tagname'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: []])
+            }
+        }
+
+        stage('Build') {
+            steps {
+                script {
+                    // Install and restore .NET dependencies
+                    sh "dotnet restore"
+                    // Build the .NET project
+                    sh "dotnet build"
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                    // Run your tests here
+                    sh "dotnet test"
+                }
+            }
+        }
+
+        stage('Containerize') {
+            steps {
+                script {
+                    // Build a Docker image for your .NET application
+                    sh "docker build -t bsaksham/dotnetwebapp:tagname/dotnetwebapp:1.0 ."
+                    // Push the Docker image to a Docker registry
+                    sh "docker push bsaksham/dotnetwebapp:tagname/dotnetwebapp:1.0"
+                }
+            }
+        }
+
+    }
+}
